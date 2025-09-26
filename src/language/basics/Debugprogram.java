0cf5036e7f12f5448967d93745d2435f7f3fefe2
Sailaja <sailaja.s2122@gmail.com>
2025-09-26 08:23:41 +0530

    Initial commit

---
+++ src/language/basics/Debugprogram.java
@@ -0,0 +1,14 @@
+package language.basics;
+
+public class Debugprogram {
+	
+	public static void main(String[] args) {
+		int i=10;
+		System.out.println("Sailaja");
+		int j=i-2;
+		System.out.println(j);
+		
+		
+	}
+
+}
